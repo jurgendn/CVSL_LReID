eca50924e4141ebe644524c6a6ae6e28262cba18
Jurgen <nguyentiendung7299@gmail.com>
2023-03-06 16:26:02 +0700

    [Person ReID][Shape Embedding] Add shape embedding [1]X. Qian et al., Long-Term Cloth-Changing Person Re-identification

---
+++ src/models/pose/shape_embedding.py
@@ -0,0 +1,113 @@
+"""
+These modules are built based on this paper
+[1]X. Qian et al., Long-Term Cloth-Changing Person Re-identification
+
+In this module, the Relation Network module use
+Graph Convolutional Network instead of Convolutional Layer
+"""
+
+from typing import List, Tuple
+import torch_geometric.nn as gnn
+
+from torch import nn, Tensor
+from torch.nn import functional as F
+
+
+class PositionEmbedding(nn.Module):
+
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super(PositionEmbedding, self).__init__()
+        self.position_embedding = nn.Linear(in_features=in_features,
+                                            out_features=out_features)
+
+    def forward(self, pose: Tensor) -> Tensor:
+        x = self.position_embedding(pose)
+        return x
+
+
+class SemanticEmbedding(nn.Module):
+
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super(SemanticEmbedding, self).__init__()
+        self.semantic_embedding = nn.Linear(in_features=in_features,
+                                            out_features=out_features)
+
+    def forward(self, sem: Tensor) -> Tensor:
+        y = self.semantic_embedding(sem)
+        return y
+
+
+class RefineNetwork(nn.Module):
+
+    def __init__(self, pose_n_features: int, sem_n_features: int,
+                 n_hidden: int, out_features: int) -> None:
+        super(RefineNetwork, self).__init__()
+        self.position_embedding = PositionEmbedding(
+            in_features=pose_n_features, out_features=n_hidden)
+        self.semantic_embedding = SemanticEmbedding(in_features=sem_n_features,
+                                                    out_features=n_hidden)
+        self.fc = nn.Linear(in_features=n_hidden, out_features=out_features)
+
+    def forward(self, p: Tensor, s: Tensor):
+        x_pos = self.position_embedding(p)
+        x_sem = self.semantic_embedding(s)
+        x = x_pos + x_sem
+        x = F.leaky_relu(x)
+        y = self.fc(x)
+        return y
+
+
+class RelationNetwork(nn.Module):
+
+    def __init__(self, layers: List[int]) -> None:
+        super(RelationNetwork, self).__init__()
+        self.__num_modules = len(layers)
+        for i, (in_channel, out_channel) in enumerate(layers):
+            setattr(
+                self, f"gcn_{i+1}",
+                gnn.GCNConv(in_channels=in_channel, out_channels=out_channel))
+
+    def forward(self, x: Tensor, a: Tensor):
+        """
+        forward
+
+        Parameters
+        ----------
+        x : Tensor
+            Node features, 2D Tensor
+        a : Tensor
+            Edge index, in format
+            [[0, 1, 1, 2, 1, 9], [1, 0, 2, 1, 8, 1]]
+
+        Returns
+        -------
+        Tensor
+            Node embedding, (n, out_channels)
+        """
+        for i in range(self.__num_modules):
+            module = getattr(self, f"gcn_{i+1}")
+            x = module(x, a)
+            x = F.leaky_relu(x)
+        return x
+
+
+class ShapeEmbedding(nn.Module):
+
+    def __init__(self, pose_n_features: int, sem_n_features: int,
+                 n_hidden: int, out_features: int,
+                 relation_layers: List[Tuple[int]]) -> None:
+        super(ShapeEmbedding, self).__init__()
+        assert out_features == relation_layers[0][0]
+        self.refine_net = RefineNetwork(pose_n_features=pose_n_features,
+                                        sem_n_features=sem_n_features,
+                                        n_hidden=n_hidden,
+                                        out_features=out_features)
+        self.relation_net = RelationNetwork(layers=relation_layers)
+        self.graph_pooling = gnn.MeanAggregation()
+
+    def forward(self, pose: Tensor, semantic: Tensor,
+                edge_index: Tensor) -> Tensor:
+        pose_features = self.refine_net(p=pose, s=semantic)
+        relation_features = self.relation_net(x=pose_features, a=edge_index)
+        graph_representation = self.graph_pooling(x=relation_features)
+        return graph_representation
