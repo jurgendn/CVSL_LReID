f36916af45c3f576fdb2aaf3237190592c3d724d
Nguyen Tien Dung <dungnt@hiworld.com.vn>
2023-04-13 23:49:32 +0700

    [PersonREID][Baseline Model] Add FTNet, ShapeEmbedding and a Baseline Model

---
+++ src/models/baseline.py
@@ -0,0 +1,92 @@
+from typing import Dict, List, Tuple
+
+import torch
+from pytorch_lightning import LightningModule
+from torch import nn, optim
+from torch.nn import functional as F
+
+from src.models.modules.r50 import FTNet
+from src.models.modules.shape_embedding import ShapeEmbedding
+
+
+class LitModule(LightningModule):
+
+    def __init__(
+        self,
+        shape_semantic: torch.FloatTensor,
+        shape_edge_index: torch.LongTensor,
+        r50_class_num: int = 751,
+        r50_droprate: float = 0.5,
+        r50_stride: int = 2,
+        r50_circle: bool = False,
+        r50_ibn: bool = False,
+        r50_linear_num: int = 512,
+        shape_pose_n_features: int = 4,
+        shape_sem_n_features: int = 33,
+        shape_n_hidden: int = 1024,
+        shape_out_features: int = 512,
+        shape_relation_layers: List[Tuple[int]] = [(512, 256), (256, 128)]
+    ) -> None:
+        super(LitModule, self).__init__()
+        self.shape_semantic = shape_semantic
+        self.shape_edge_index = shape_edge_index
+        self.ft_net = FTNet(class_num=r50_class_num,
+                            droprate=r50_droprate,
+                            stride=r50_stride,
+                            circle=r50_circle,
+                            ibn=r50_ibn,
+                            linear_num=r50_linear_num)
+        self.shape_embedding = ShapeEmbedding(
+            pose_n_features=shape_pose_n_features,
+            sem_n_features=shape_sem_n_features,
+            n_hidden=shape_n_hidden,
+            out_features=shape_out_features,
+            relation_layers=shape_relation_layers)
+        self.fusion = nn.Linear(in_features=1,
+                                out_features=1)  # Định nghĩa sau
+        self.id_classification = nn.Linear(in_features=1024, out_features=751)
+
+    def forward(self, x_image: torch.Tensor,
+                x_pose_features: torch.FloatTensor,
+                x_semantic_features: torch.FloatTensor,
+                edge_index: torch.LongTensor) -> torch.Tensor:
+        appearance_feature = self.ft_net(x=x_image)
+        pose_feature = self.shape_embedding(pose=x_pose_features,
+                                            semantic=x_semantic_features,
+                                            edge_index=edge_index)
+
+        fusion_feature = self.fusion(appreance=appearance_feature,
+                                     pose=pose_feature)
+        return fusion_feature
+
+    def configure_optimizers(self, lr: float):
+        optimizer = optim.Adam(params=self.parameters(), lr=lr)
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        (a_img, p_img, n_img), (a_pose, p_pose, n_pose), a_id = batch
+        a_features = self.forward(x_image=a_img,
+                                  x_pose_features=a_pose,
+                                  x_semantic_features=self.shape_semantic,
+                                  edge_index=self.shape_edge_index)
+        p_features = self.forward(x_image=p_img,
+                                  x_pose_features=p_pose,
+                                  x_semantic_features=self.shape_semantic,
+                                  edge_index=self.shape_edge_index)
+        n_features = self.forward(x_image=n_img,
+                                  x_pose_features=n_pose,
+                                  x_semantic_features=self.shape_semantic,
+                                  edge_index=self.shape_edge_index)
+
+        triplet_loss = F.triplet_margin_loss(anchor=a_features,
+                                             positive=p_features,
+                                             negative=n_features)
+        logits = self.id_classification(a_features)
+        id_loss = F.cross_entropy(logits, a_id)
+        return {'loss': id_loss + triplet_loss, 'extras': []}
+
+    def on_train_batch_end(self, outputs: Dict):
+        return outputs
+
+    def on_train_epoch_end(self) -> None:
+        self.log(name="train/f1", value=1)
---
+++ src/models/modules/r50.py
@@ -0,0 +1,224 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn import init
+from torchvision import models
+
+
+######################################################################
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    # print(classname)
+    if classname.find('Conv') != -1:
+        init.kaiming_normal_(
+            m.weight.data, a=0,
+            mode='fan_in')  # For old pytorch, you may use kaiming_normal.
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
+    elif classname.find('BatchNorm1d') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+    if hasattr(m, 'bias') and m.bias is not None:
+        init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        init.normal_(m.weight.data, std=0.001)
+        init.constant_(m.bias.data, 0.0)
+
+
+def activate_drop(m):
+    classname = m.__class__.__name__
+    if classname.find('Drop') != -1:
+        m.p = 0.1
+        m.inplace = True
+
+
+# Defines the new fc layer and classification layer
+# |--Linear--|--bn--|--relu--|--Linear--|
+class ClassBlock(nn.Module):
+
+    def __init__(self,
+                 input_dim,
+                 class_num,
+                 droprate,
+                 relu=False,
+                 bnorm=True,
+                 linear=512,
+                 return_f=False):
+        super(ClassBlock, self).__init__()
+        self.return_f = return_f
+        add_block = []
+        if linear > 0:
+            add_block += [nn.Linear(input_dim, linear)]
+        else:
+            linear = input_dim
+        if bnorm:
+            add_block += [nn.BatchNorm1d(linear)]
+        if relu:
+            add_block += [nn.LeakyReLU(0.1)]
+        if droprate > 0:
+            add_block += [nn.Dropout(p=droprate)]
+        add_block = nn.Sequential(*add_block)
+        add_block.apply(weights_init_kaiming)
+
+        classifier = []
+        classifier += [nn.Linear(linear, class_num)]
+        classifier = nn.Sequential(*classifier)
+        classifier.apply(weights_init_classifier)
+
+        self.add_block = add_block
+        self.classifier = classifier
+
+    def forward(self, x):
+        x = self.add_block(x)
+        if self.return_f:
+            f = x
+            x = self.classifier(x)
+            return [x, f]
+        else:
+            x = self.classifier(x)
+            return x
+
+
+# Define the ResNet50-based Model
+class FTNet(nn.Module):
+
+    def __init__(self,
+                 class_num=751,
+                 droprate=0.5,
+                 stride=2,
+                 circle=False,
+                 ibn=False,
+                 linear_num=512):
+        super(FTNet, self).__init__()
+        model_ft = models.resnet50(pretrained=True)
+        if ibn == True:
+            model_ft = torch.hub.load('XingangPan/IBN-Net',
+                                      'resnet50_ibn_a',
+                                      pretrained=True)
+        # avg pooling to global pooling
+        if stride == 1:
+            model_ft.layer4[0].downsample[0].stride = (1, 1)
+            model_ft.layer4[0].conv2.stride = (1, 1)
+        model_ft.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.model = model_ft
+        self.circle = circle
+        self.classifier = ClassBlock(2048,
+                                     class_num,
+                                     droprate,
+                                     linear=linear_num,
+                                     return_f=circle)
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.model.avgpool(x)
+        x = x.view(x.size(0), x.size(1))
+        x = self.classifier(x)
+        return x
+
+
+class PCB(nn.Module):
+
+    def __init__(self, class_num):
+        super(PCB, self).__init__()
+
+        self.part = 6  # We cut the pool5 to 6 parts
+        model_ft = models.resnet50(pretrained=True)
+        self.model = model_ft
+        self.avgpool = nn.AdaptiveAvgPool2d((self.part, 1))
+        self.dropout = nn.Dropout(p=0.5)
+        # remove the final downsample
+        self.model.layer4[0].downsample[0].stride = (1, 1)
+        self.model.layer4[0].conv2.stride = (1, 1)
+        # define 6 classifiers
+        for i in range(self.part):
+            name = 'classifier' + str(i)
+            setattr(
+                self, name,
+                ClassBlock(2048,
+                           class_num,
+                           droprate=0.5,
+                           linear=256,
+                           relu=False,
+                           bnorm=True))
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.avgpool(x)
+        x = self.dropout(x)
+        part = {}
+        predict = {}
+        # get six part feature batchsize*2048*6
+        for i in range(self.part):
+            part[i] = x[:, :, i].view(x.size(0), x.size(1))
+            name = 'classifier' + str(i)
+            c = getattr(self, name)
+            predict[i] = c(part[i])
+
+        # sum prediction
+        #y = predict[0]
+        #for i in range(self.part-1):
+        #    y += predict[i+1]
+        y = []
+        for i in range(self.part):
+            y.append(predict[i])
+        return y
+
+
+class PCBTest(nn.Module):
+
+    def __init__(self, model):
+        super(PCBTest, self).__init__()
+        self.part = 6
+        self.model = model.model
+        self.avgpool = nn.AdaptiveAvgPool2d((self.part, 1))
+        # remove the final downsample
+        self.model.layer4[0].downsample[0].stride = (1, 1)
+        self.model.layer4[0].conv2.stride = (1, 1)
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.avgpool(x)
+        y = x.view(x.size(0), x.size(1), x.size(2))
+        return y
+
+
+'''
+# debug model structure
+# Run this code with:
+python model.py
+'''
+if __name__ == '__main__':
+    # Here I left a simple forward function.
+    # Test the model, before you train it.
+    net = FTNet(751)
+    #net = FTNet_swin(751, stride=1)
+    net.classifier = nn.Sequential()
+    print(net)
+    input = torch.FloatTensor(8, 3, 224, 224)
+    output = net(input)
